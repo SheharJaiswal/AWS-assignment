746880e5e8cf2bbfba8e71f841c9260a0ab11063
Shekhar jaiswal <121437523+SheharJaiswal@users.noreply.github.com>
2026-09-23 01:22:50 +0530

    feat: add src/AwsDocumentPortal/Controllers/HomeController.cs

---
+++ src/AwsDocumentPortal/Controllers/HomeController.cs
@@ -0,0 +1,3 @@
+using Microsoft.AspNetCore.Mvc;
+namespace AwsDocumentPortal.Controllers;
+public sealed class HomeController:Controller{public IActionResult Index()=>View();public IActionResult Error()=>View();}
