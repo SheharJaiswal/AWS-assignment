10888275216e72680156fd16618ade08e9dacc9b
Shekhar jaiswal <121437523+SheharJaiswal@users.noreply.github.com>
2026-09-23 01:22:35 +0530

    feat: add src/AwsDocumentPortal/Program.cs

---
+++ src/AwsDocumentPortal/Program.cs
@@ -0,0 +1,9 @@
+using AwsDocumentPortal.Services;
+var builder=WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IDocumentStorage,LocalDocumentStorage>();
+var app=builder.Build();
+if(!app.Environment.IsDevelopment()){app.UseExceptionHandler("/Home/Error");app.UseHsts();}
+app.UseHttpsRedirection();app.UseStaticFiles();app.UseRouting();app.UseAuthorization();
+app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+app.Run();
