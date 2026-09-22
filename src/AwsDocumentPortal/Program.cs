7b563ff251529c4e4490f3f1f7355395699bd704
Shekhar jaiswal <121437523+SheharJaiswal@users.noreply.github.com>
2026-09-23 02:58:12 +0530

    fix: restore AWS application source structure

--- src/AwsDocumentPortal/Program.cs
+++ src/AwsDocumentPortal/Program.cs
@@ -1,14 +1,34 @@
+using Amazon;
 using Amazon.S3;
 using AwsDocumentPortal.Services;
-var builder=WebApplication.CreateBuilder(args);
+
+var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
-builder.Services.AddAWSService<IAmazonS3>();
-builder.Services.AddSingleton<IDocumentStorage,LocalDocumentStorage>();
-builder.Services.AddSingleton<IS3DocumentStorage,S3DocumentStorage>();
-builder.Services.AddSingleton<IDocumentStorage,StorageProviderDocumentStorage>();
-var app=builder.Build();
-if(!app.Environment.IsDevelopment()){app.UseExceptionHandler("/Home/Error");app.UseHsts();}
-app.UseHttpsRedirection();app.UseStaticFiles();app.UseRouting();app.UseAuthorization();
-app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+
+var regionName = builder.Configuration["AWS:Region"] ?? "ap-south-1";
+builder.Services.AddSingleton<IAmazonS3>(_ =>
+    new AmazonS3Client(RegionEndpoint.GetBySystemName(regionName)));
+
+builder.Services.AddSingleton<ILocalDocumentStorage, LocalDocumentStorage>();
+builder.Services.AddSingleton<IS3DocumentStorage, S3DocumentStorage>();
+builder.Services.AddSingleton<IDocumentStorage, StorageProviderDocumentStorage>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
