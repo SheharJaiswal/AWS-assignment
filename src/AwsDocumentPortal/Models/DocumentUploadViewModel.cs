1ceec7c2c493ebb53d481369fa800e480352c2a8
Shekhar jaiswal <121437523+SheharJaiswal@users.noreply.github.com>
2026-09-23 01:22:40 +0530

    feat: add src/AwsDocumentPortal/Models/DocumentUploadViewModel.cs

---
+++ src/AwsDocumentPortal/Models/DocumentUploadViewModel.cs
@@ -0,0 +1,3 @@
+using System.ComponentModel.DataAnnotations;
+namespace AwsDocumentPortal.Models;
+public sealed class DocumentUploadViewModel{[Required,Display(Name="Document type")]public string DocumentType{get;set;}="Identity Proof";[Required,Display(Name="Document")]public IFormFile? File{get;set;}}
