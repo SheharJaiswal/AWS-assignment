baae7df6e06e51124f925438831f478e1db1df09
Shekhar jaiswal <121437523+SheharJaiswal@users.noreply.github.com>
2026-09-23 02:30:21 +0530

    feat: add AWS S3 document storage

--- src/AwsDocumentPortal/Program.cs
+++ src/AwsDocumentPortal/Program.cs
@@ -1,7 +1,12 @@
+using Amazon.S3;
 using AwsDocumentPortal.Services;
 var builder=WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonS3>();
 builder.Services.AddSingleton<IDocumentStorage,LocalDocumentStorage>();
+builder.Services.AddSingleton<IS3DocumentStorage,S3DocumentStorage>();
+builder.Services.AddSingleton<IDocumentStorage,StorageProviderDocumentStorage>();
 var app=builder.Build();
 if(!app.Environment.IsDevelopment()){app.UseExceptionHandler("/Home/Error");app.UseHsts();}
 app.UseHttpsRedirection();app.UseStaticFiles();app.UseRouting();app.UseAuthorization();
